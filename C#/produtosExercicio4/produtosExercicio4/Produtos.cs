776475bc23acbca16adf43744172b7fe63f0b315
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-13 22:43:16 -0300

    Exercicio4 utilizando a Classe Produtos com os atributos codigo, descricao, estoque, valorUnitário, valorDesconto, valorTaxa, valorTotal com o metodo infoProduto

---
+++ C#/produtosExercicio4/produtosExercicio4/Produtos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace produtosExercicio4
+{
+    class Produtos
+    {
+        public string codigo;
+        public string descricao;
+        public int estoque = 6;
+        public double valorUnitario;
+        double valorDesconto;
+        double valorTaxa;
+        double valorTotal;
+
+        public void infoProduto()
+        {
+            valorDesconto = valorUnitario / 100 * 5;
+            valorTaxa = valorUnitario / 100 * 10;
+            valorTotal = valorUnitario - valorDesconto + valorTaxa;
+
+            Console.WriteLine($"Código: {codigo} Descrição:{descricao} Estoque: {estoque}");
+            Console.WriteLine($"Valor unitário: {valorUnitario} Valor desconto: {valorDesconto} Valor taxa: {valorTaxa} Valor total: {valorTotal}");
+        }
+    }
+}
---
+++ C#/produtosExercicio4/produtosExercicio4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace produtosExercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produtos produtos = new Produtos();
+            produtos.codigo = "1";
+            produtos.descricao = "Cadeira";
+            produtos.valorUnitario = 10;
+
+            produtos.infoProduto();
+
+            Console.ReadKey();
+        }
+    }
+}
