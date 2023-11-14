3e780fb4e9158603a4eccba457e870347d80046b
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-13 22:33:47 -0300

    Exercicio2 utilizando a Classe Calculo e dois atributos com metodos de adicao, subtracao, multiplicacao e divisao

---
+++ C#/calculoExercicio2/calculoExercicio2/Calculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculoExercicio2
+{
+    class Calculo
+    {
+        public int n1;
+        public int n2;
+
+        public string adicao()
+        {
+            return $"Adição: {n1 + n2}";
+        }
+        public string subtracao()
+        {
+            return $"Subtração: {n1 - n2}";
+        }
+        public string multiplicacao()
+        {
+            return $"Multiplicação: {n1 * n2}";
+        }
+        public string divisao()
+        {
+            return $"Divisão: {n1 / n2}";
+        }
+    }
+}
---
+++ C#/calculoExercicio2/calculoExercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace calculoExercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculo calculo = new Calculo();
+            calculo.n1 = 1;
+            calculo.n2 = 2;
+
+            Console.WriteLine(calculo.adicao());
+            Console.WriteLine(calculo.subtracao());
+            Console.WriteLine(calculo.divisao());
+            Console.WriteLine(calculo.multiplicacao());
+        }
+    }
+}
