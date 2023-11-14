d3b1129e4241eb41b5426cd82a3df98623633f33
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-13 22:38:14 -0300

    Exercicio3 utilizando a Classe Retangulo com atributos de altura e comprimento e os metodos de mostrarArea e mostrarPerimetro

---
+++ C#/retanguloExercicio3/retanguloExercicio3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace retanguloExercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            retangulo.altura = 5;
+            retangulo.comprimento = 10;
+
+            Console.WriteLine(retangulo.mostrarPerimetro());
+            Console.WriteLine(retangulo.mostrarArea());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/retanguloExercicio3/retanguloExercicio3/Retangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace retanguloExercicio3
+{
+    class Retangulo
+    {
+        public double comprimento;
+        public double altura;
+
+        public string mostrarPerimetro()
+        {
+            return $"Perímetro: {comprimento * 2 + altura * 2}";
+        }
+        public string mostrarArea()
+        {
+            return $"Área: {comprimento * altura}";
+        }
+    }
+}
