4454e0a6eb7f2218f6e2331d194166f598fe1a79
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-13 23:07:46 -0300

    Modificadores de Acesso criando a Class Carro com o campo marca private e com o campo modelo public

---
+++ C#/modificadorDeAcesso/modificadorDeAcesso/Carro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modificadorDeAcesso
+{
+    class Carro
+    {
+        private string marca = "Fiat";
+        public string modelo = "Uno";
+    }
+}
---
+++ C#/modificadorDeAcesso/modificadorDeAcesso/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace modificadorDeAcesso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            
+            //Console.WriteLine(carro.marca); Não é possível ter acesso
+            Console.WriteLine(carro.modelo);
+        }
+    }
+}
