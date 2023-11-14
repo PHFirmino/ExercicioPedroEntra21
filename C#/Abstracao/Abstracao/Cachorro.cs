9a48872aaeb7507c13fd5eb5d91c0a6de2f7e663
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-14 01:15:01 -0300

    Abstracao,  criando uma class abstract Animal e criando uma class Cachorro que herdou o metodo da class abstract Animal, nao eh possivel criar um objeto da class Animal, apenas da class Cachorro onde consigo acessar o metodo da class abstract Animal

---
+++ C#/Abstracao/Abstracao/Cachorro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracao
+{
+    class Cachorro : Animal
+    {
+        public void sono() 
+        {
+            Console.WriteLine("ZzZzZzZ");
+        }
+    }
+}
---
+++ C#/Abstracao/Abstracao/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Abstracao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Animal animal = new Animal(); Não consigo acessar
+
+            Cachorro cachorro = new Cachorro();
+            cachorro.dormir();
+            cachorro.sono();
+
+            Console.ReadKey();
+        }
+    }
+}
