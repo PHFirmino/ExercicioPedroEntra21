e477b20828dea1d5d1d64e0b9f45c2627f442f07
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-14 01:26:00 -0300

    Interfaces, criando a interface IAnimal com os metodos dormir, comer, beber e criando a class Gato que herdou da interface IAnimal os metodos dormir, comer, beber

---
+++ C#/interfaces_/interfaces_/IAnimal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static interfaces_.IAnimal;
+
+namespace interfaces_
+{
+    interface IAnimal
+    {
+        void dormir();
+        void comer();
+        void beber();
+    }
+
+    class Gato : IAnimal
+    {
+        public void beber()
+        {
+            Console.WriteLine("glub, glub");
+        }
+
+        public void comer()
+        {
+            Console.WriteLine("inhec, inhec");
+        }
+
+        public void dormir()
+        {
+            Console.WriteLine("ZzZzZ");
+        }
+    }
+
+}
---
+++ C#/interfaces_/interfaces_/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace interfaces_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gato gato = new Gato();
+            gato.dormir();
+            gato.comer();
+            gato.beber();
+
+            Console.ReadKey();
+        }
+    }
+}
