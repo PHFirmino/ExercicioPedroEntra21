a483b995d3b5af9f124e7eef6ad1befcad6033df
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-14 00:59:53 -0300

    Polimorfismo, criando a class Veículo com o metodo som adicionando a palavra virtual no metodo e criando a class Moto que herda o metodo som da class Veiculo adicionando a palavra override no metodo

---
+++ C#/polimorfismo/polimorfismo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace polimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo veiculo = new Veiculo();
+            veiculo.som();
+
+            Veiculo moto = new Moto();
+            moto.som();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/polimorfismo/polimorfismo/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polimorfismo
+{
+    class Veiculo
+    {
+        public virtual void som()
+        {
+            Console.WriteLine("Vrum");
+        }
+    }
+    class Moto : Veiculo
+    {
+        public override void som()
+        {
+            Console.WriteLine("Bén");
+        }
+    }
+
+}
