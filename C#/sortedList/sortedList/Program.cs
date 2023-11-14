c1977c8cfc4a863cea6c8e5bc1e1dc5cd24d8d09
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-14 02:06:55 -0300

    SortedList, criando uma SortedList, adicionando valores para Key e Value e percorrendo por ela

---
+++ C#/sortedList/sortedList/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace sortedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            colecaoKeyValue();
+
+            Console.ReadKey();
+        }
+        static void colecaoKeyValue()
+        {
+            SortedList<string, string> keyValue = new SortedList<string, string>();
+            keyValue.Add("txt", "notepad.exe");
+            keyValue.Add("doc", "winword.exe");
+
+            Console.WriteLine(keyValue.Values.ToArray()[0]);
+            Console.WriteLine(keyValue.Keys.ToArray()[0]);
+
+            Object[] valores = keyValue.Values.ToArray();
+
+            //for (int i = 0; i < valores.Length; i++) 
+            //{
+            //    Console.WriteLine(valores[i]);
+            //}
+
+            foreach (KeyValuePair<string, string> pair in keyValue)
+            {
+                Console.WriteLine(pair.Key);
+                Console.WriteLine(pair.Value);
+            }
+        }
+    }
+}
