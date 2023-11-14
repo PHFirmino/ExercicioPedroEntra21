4e828c0dd6b103f6c3696357cbb54a9560afbef6
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-14 01:42:34 -0300

    Exercicio de Herenca e Polimorfismo, criando a class Veiculo com propriedades de Marca e Modelo e o metodo som com a palavra virutal, a partir dela eh criada a class Motos com a propriedade Cilindradas que herdou as propriedades e o metodo da class Veiculo, depois eh criada as classes MotoCorrida e MotoNormal com propriedades especificas, utilizam das propriedades e metodo da class Veiculo e utilizam propriedade da class Motos

---
+++ C#/polimorfismoHerencaExercicio/polimorfismoHerencaExercicio/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace polimorfismoHerencaExercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MotoCorrida motoCorrida = new MotoCorrida("Ducat", "Pista", "Speed", 100);
+            Console.WriteLine(motoCorrida.som());
+
+            MotoNormal motoNormal = new MotoNormal("Honda", "Inox", 200, "Honda-200");
+            Console.WriteLine(motoNormal.som());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/polimorfismoHerencaExercicio/polimorfismoHerencaExercicio/Veiculos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polimorfismoHerencaExercicio
+{
+    abstract class Veiculos
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+
+        public virtual string som()
+        {
+            return "Vrumm";
+        }
+    }
+    class Motos : Veiculos
+    {
+        public int Cilindradas { get; set; }
+    }
+    class MotoCorrida : Motos
+    {
+        public string Estilo { get; set; }
+
+        public MotoCorrida(string marca, string estilo, string modelo, int cilindradas)
+        {
+            this.Marca = marca;
+            this.Estilo = estilo;
+            this.Modelo = modelo;
+            this.Cilindradas = cilindradas;
+        }
+
+        public override string som()
+        {
+            return "Dééénn";
+        }
+    }
+    class MotoNormal : Motos
+    {
+        public string Escapamento { get; set; }
+
+        public MotoNormal(string marca, string escapamento, int cilindradas, string modelo)
+        {
+            this.Marca = marca;
+            this.Cilindradas = cilindradas;
+            this.Modelo = modelo;
+            this.Escapamento = escapamento;
+        }
+
+        public override string som()
+        {
+            return "Bééénnn";
+        }
+    }
+}
