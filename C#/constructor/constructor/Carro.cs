8f02ccb597e909838f8a40b7cced4db607bfe02b
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-13 22:54:31 -0300

    Metodos construtores, criando a Class Carro, criando o Construtor Carro com atributos marcas, modelos, placas e cores com o metodo mostrarCarro

---
+++ C#/constructor/constructor/Carro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor
+{
+    class Carro
+    {
+        string marcas;
+        string modelos;
+        string placas;
+        string cores;
+        public Carro(string marca, string modelo, string placa, string cor) 
+        {
+            marcas = marca;
+            modelos = modelo;
+            placas = placa;
+            cores = cor;
+        }
+        public string mostrarCarro()
+        {
+            return $"Marca: {marcas}, Modelo: {modelos}, Placa: {placas} Cor: {cores}";
+        }
+    }
+}
---
+++ C#/constructor/constructor/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro("Fiat", "Uno", "ABC-123", "Preto");
+            Console.WriteLine(carro.mostrarCarro());
+
+            Console.ReadKey();
+        }
+    }
+}
