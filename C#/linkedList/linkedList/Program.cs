9c2057b0de80cee4055e6cd727b9d2ad71402e27
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-14 02:03:29 -0300

    LinkedList, criando uma LinkedList, adicionando valores e percorrendo por ela

---
+++ C#/linkedList/linkedList/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace linkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            colecaoLinkedList();
+
+            Console.ReadKey();
+        }
+        static void colecaoLinkedList()
+        {
+            LinkedList<string> lista = new LinkedList<string>();
+            lista.AddFirst("Pedro");
+            lista.AddFirst("Maria");
+            lista.AddLast("Lucas");
+
+            mostrarLista(lista);
+
+            LinkedListNode<string> x = lista.Find("Pedro");
+            Console.WriteLine(x.Value);
+
+            LinkedListNode<string> anterior = x.Previous;
+            Console.WriteLine(anterior.Value);
+
+            LinkedListNode<string> proximo = x.Next;
+            Console.WriteLine(proximo.Value);
+
+            lista.AddAfter(x, "João");
+            lista.AddBefore(x, "Sandro");
+        }
+        static void mostrarLista(LinkedList<string> lista)
+        {
+            foreach (string item in lista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
