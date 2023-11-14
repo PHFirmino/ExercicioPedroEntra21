69ead3acedd5704d9230deab679e47d00f9a22e7
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-13 22:24:07 -0300

    Exercicio1 utilizando a classe Carro com os atributos placa, marca, modelo e cor e o metodo display

---
+++ C#/carroExercicio1/carroExercicio1/Carro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carroExercicio1
+{
+    class Carro
+    {
+        public string placa;
+        public string marca;
+        public string modelo;
+        public string cor;
+
+        public string display()
+        {
+            return $"Placa: {placa}, Marca: {marca}, Modelo: {modelo}, Cor: {cor}";
+        }
+    }
+}
---
+++ C#/carroExercicio1/carroExercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace carroExercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            carro.placa = "ABC-123";
+            carro.marca = "Fiat";
+            carro.modelo = "Uno";
+            carro.cor = "Preto";
+
+            Console.WriteLine(carro.display());
+            Console.ReadKey();
+        }
+    }
+}
