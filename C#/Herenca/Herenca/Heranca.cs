40621a4f66e4f355dc078eba9c3e674dc4344fad
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-14 00:47:07 -0300

    Heranca, criando a class Veiculo e criando a class Carro que herdou metodos e campos da class Veiculo também criando a class Pessoa e criando a class Cliente que herdou metodos e campos da class Pessoa

---
+++ C#/Herenca/Herenca/Heranca.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herenca
+{
+    class Veiculo
+    {
+        public string Marca { get; set; }
+        public virtual void barulho()
+        {
+            Console.WriteLine("Vrummm");
+        }
+    }
+    class Carro : Veiculo
+    { 
+        public string Modelo { get; set; }
+
+        public override void barulho()
+        {
+            Console.WriteLine("Vrummm carro");
+        }
+    }
+
+    class Pessoa
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public string mostrarPessoa()
+        {
+            return $"{Id} {Name}";
+        }
+    }
+    class Cliente : Pessoa 
+    {
+        public string Cpf { get; set; }
+
+        public string mostrarPessoa()
+        {
+            string msg = base.mostrarPessoa();
+
+            return $"{msg} {Cpf}";
+
+        }
+    }
+}
---
+++ C#/Herenca/Herenca/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Herenca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            carro.Marca = "Fiat";
+            carro.Modelo = "Uno";
+
+            carro.barulho();
+
+            Cliente cliente = new Cliente();
+
+            cliente.Name = "Pedro";
+            cliente.Id = "123";
+            cliente.Cpf = "111111";
+
+            Console.WriteLine(cliente.mostrarPessoa());
+
+            Console.ReadKey();
+        }
+    }
+}
