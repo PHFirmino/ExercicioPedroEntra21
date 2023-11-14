17cf4af3b05ca89a408214d539b8a9bc351d3e55
PHFirmino <pedromestrenoclash123@gmail.com>
2023-11-13 23:38:14 -0300

    Criando List, adicionando item, removendo item e percorrendo pela List

---
+++ C#/listas/listas/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("Pedro");
+            list.Add("João");
+            list.Remove("João");
+            
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
